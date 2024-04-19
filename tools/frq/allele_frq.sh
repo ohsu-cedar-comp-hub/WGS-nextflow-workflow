27117a87e13c9421b7fc89c30a53f3b46b850310
Jordan Lee <jordanl.7890@gmail.com>
2024-04-19 14:21:18 -0700

    run other vcf checks

--- tools/frq/allele_frq.sh
+++ tools/frq/allele_frq.sh
@@ -7,6 +7,14 @@
 VCF=${1}
 OUT=${2}
 
-# Allele freq. exclude sites with more than 2 alleles
+# Allele freq. exclude sites with more than 2 alleles (creates .frq)
 echo 'Running allele freq'
 vcftools --gzvcf $VCF --freq2 --out $OUT --max-alleles 2
+
+# site quality (creates .lqual)
+echo 'Running site qual'
+vcftools --gzvcf $VCF --site-quality --out $OUT
+
+# mean depth per site (creates *.ldepth.mean)
+echo 'Running mean depth per site'
+vcftools --gzvcf $SUBSET_VCF --site-mean-depth --out $OUT
